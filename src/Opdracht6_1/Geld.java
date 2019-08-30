0976525fc2e9b0390a1d71f90c7dabbed52a2f8b
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-08-30 19:01:13 +0200

    eerste commit

--- src/Opdracht6_1/Geld.java
+++ src/Opdracht6_1/Geld.java
@@ -1,4 +1,21 @@
 package Opdracht6_1;
 
-public class Geld {
+import java.awt.*;
+import java.applet.*;
+
+public class Geld extends Applet {
+    double personen, hoeveelheid, uitkomst;
+
+    public void init() {
+        personen = 4;
+        hoeveelheid = 113;
+        uitkomst = hoeveelheid / personen;
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("Jan: " + uitkomst, 20, 20);
+        g.drawString("Ali: " + uitkomst, 20,40);
+        g.drawString("Jeanette: " + uitkomst, 20,60);
+        g.drawString("Namara: " +uitkomst,20,80);
+    }
 }
