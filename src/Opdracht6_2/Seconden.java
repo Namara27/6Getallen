c230188c4e05870e464ddaecd799625b0d1f4196
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-08-30 19:27:43 +0200

    tweede commit

--- src/Opdracht6_2/Seconden.java
+++ src/Opdracht6_2/Seconden.java
@@ -1,4 +1,22 @@
 package Opdracht6_2;
 
-public class Seconden {
+import java.awt.*;
+import java.applet.*;
+
+public class Seconden extends Applet {
+    int uur;
+    int dag;
+    int jaar;
+
+    public void init() {
+        uur = 60 * 60;
+        dag = 24 * uur;
+        jaar = 365 * dag;
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("In een uur zitten " + uur + " seconden",20,20);
+        g.drawString("In een dag zitten " + dag + " seconden",20,40);
+        g.drawString("In een jaar zitten " + jaar + " seconden",20,60);
+    }
 }
