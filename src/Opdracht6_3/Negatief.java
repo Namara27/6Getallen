96a6334941d1de8ae0850fa2ea06f805c521c679
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-08-30 19:37:04 +0200

    tweede commit

--- src/Opdracht6_3/Negatief.java
+++ src/Opdracht6_3/Negatief.java
@@ -1,4 +1,17 @@
 package Opdracht6_3;
 
-public class Negatief {
+import java.awt.*;
+import java.applet.*;
+
+public class Negatief extends Applet {
+    int positief;
+
+    public void init() {
+        positief = 2147483647;
+    }
+
+    public void paint(Graphics g) {
+        positief++;
+        g.drawString("" + positief,20,20);
+    }
 }
