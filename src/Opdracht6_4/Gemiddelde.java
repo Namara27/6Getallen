227de884d9750705d35eea7caf6fc3739983ca1d
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-08-30 19:41:35 +0200

    tweede commit

---
+++ src/Opdracht6_4/Gemiddelde.java
@@ -0,0 +1,15 @@
+package Opdracht6_4;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Gemiddelde extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        
+    }
+}
